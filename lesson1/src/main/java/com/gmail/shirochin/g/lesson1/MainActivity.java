1e4dc7292c99d57a6628074049c21831c084d6d3
Georgy.Shirochin <g.shirochin@gmail.com>
2019-12-11 14:28:44 +0300

    Initial commit

---
+++ lesson1/src/main/java/com/gmail/shirochin/g/lesson1/MainActivity.java
@@ -0,0 +1,22 @@
+package com.gmail.shirochin.g.lesson1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    protected void onStart() {
+        super.onStart();
+    }
+    protected void onStop() {
+        super.onStop();
+    }
+
+}
